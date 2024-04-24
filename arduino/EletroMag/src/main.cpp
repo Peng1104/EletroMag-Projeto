b75ef787ca303a88476ebdba145addfeabbb9bce
Peng1104 <admin@peng1104.net>
2024-04-24 10:06:40 -0300

    Doc Fix

--- arduino/EletroMag/src/main.cpp
+++ arduino/EletroMag/src/main.cpp
@@ -7,7 +7,7 @@ void setup() {
     // Configure Timer 1 for PWM FREQUENCY on pin 10 = PB2 = OC1B.
     DDRB  |= _BV(PB2);    // set pin as output
     TCCR1B = 0;           // stop timer
-    TCCR1A = _BV(COM1B1)  // non-inverting PWN on OC1A
+    TCCR1A = _BV(COM1B1)  // non-inverting PWN on OC1B
            | _BV(WGM11);  // mode 14: fast PWM, TOP = ICR1
     TCCR1B = _BV(WGM12)   // ditto
            | _BV(WGM13)   // ditto
